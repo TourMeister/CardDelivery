c9303e305ff6633480525021ee8448f0c83619de
hYxA <zybu@live.ru>
2021-11-08 22:18:57 +0300

    еуые зфыыув shouldOrderCardDeliveryByText

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,58 +1,61 @@
 package ru.netology;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import ru.netology.manager.TimeManager;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
 
 public class CardDeliveryTest {
     private WebDriver driver;
+    TimeManager manager = new TimeManager();
+    String dateToInput = manager.TimeConstructor();
+
+    @BeforeAll
+//    Автоматическая установка ChromeDriver для операционной системы
+    static void setUpClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        ChromeOptions options = new ChromeOptions();
+//        driver = new ChromeDriver(options);
+        open("http://localhost:9999");
+    }
+
+    @AfterEach
+    void teardown() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 
     @Test
-    void print() {
-        TimeManager manager = new TimeManager();
-        String futureDate = manager.TimeConstructor();
-        System.out.println(manager);
+    void shouldOrderCardDeliveryByText() {
+        $("[placeholder='Город']").setValue("Мурманск");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL+"a");
+        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
+        $("[placeholder='Дата встречи']").setValue(String.valueOf(dateToInput));
+        $("[name='name']").setValue("Пореченков Михаил");
+        $("[name='phone']").setValue("+79009009988");
+        $("[class='checkbox__box']").click();
+        $(withText("Забронировать")).click();
+
+        $(withText("Успешно!")).
+                shouldBe(visible, Duration.ofSeconds(15));
     }
-//
-//    @BeforeAll
-////    Автоматическая установка ChromeDriver для операционной системы
-//    static void setUpClass() {
-//        WebDriverManager.chromedriver().setup();
-//    }
-
-//    @BeforeEach
-//    void setupTest() {
-//        ChromeOptions options = new ChromeOptions();
-////        driver = new ChromeDriver(options);
-//        open("http://localhost:9999");
-//
-//    }
-//
-//    @AfterEach
-//    void teardown() {
-//        if (driver != null) {
-//            driver.quit();
-//            driver = null;
-//        }
-//    }
-
-//
-//    @Test
-//    void shouldOrderCardDeliveryByText() {
-//        $("[placeholder='Город']").setValue("Мурманск");
-//        $("[placeholder='Дата встречи']").sendKeys(Keys.CONTROL+"a");
-//        $("[placeholder='Дата встречи']").sendKeys(Keys.DELETE);
-//        $("[placeholder='Дата встречи']").setValue(String.valueOf(dtPlus));
-//        $("[name='name']").setValue("Пореченков Михаил");
-//        $("[name='phone']").setValue("+79009009988");
-//        $(withText("checkbox__box")).click();
-//        $(withText("Забронировать")).click();
-//
-//        $(withText("Успешная регистрация! Вы будете перенаправлены в течение нескольких секунд.")).
-//                shouldBe(visible, Duration.ofSeconds(6));
-//        $(withText("Личный кабинет")).
-//                shouldBe(visible, Duration.ofSeconds(6));
-//    }
 }
